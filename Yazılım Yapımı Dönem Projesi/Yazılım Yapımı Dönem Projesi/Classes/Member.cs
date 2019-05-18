c29fafc1ef72904acf79f3604eb64bdc587f36fb
Ardanuc AKAR <akarardanuc@gmail.com>
2019-05-19 00:09:29 +0300

    User, member classları oıluşturuldu.

---
+++ Yazılım Yapımı Dönem Projesi/Yazılım Yapımı Dönem Projesi/Classes/Member.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Yazılım_Yapımı_Dönem_Projesi
+{
+    public class Member : User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Language { get; set; }
+    }
+}
---
+++ Yazılım Yapımı Dönem Projesi/Yazılım Yapımı Dönem Projesi/Classes/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Yazılım_Yapımı_Dönem_Projesi
+{
+    public abstract class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
